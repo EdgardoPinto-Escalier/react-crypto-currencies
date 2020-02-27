9c865220c0dc7e1737d825d2451e3a80bffbab56
EdgardoPinto-Escalier <ep-marketing-media@live.com>
2020-02-27 21:06:14 +0100

    Created Header component

--- src/index.js
+++ src/index.js
@@ -1,12 +1,21 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import Header from './components/common/Header';
 
 
 const App = () => {
-  return <h1>React Crypto Currencies</h1>;
+  const appTitle = 'React Crypto Currencies';
+
+  return (
+    <div>
+      <Header />
+      <h1>{appTitle}</h1>
+      <p>Best Crypto Currencies App with all up to date Info</p>
+    </div>
+  );
 }
 
 ReactDOM.render(
   <App />,
   document.getElementById('root')
-)
+);
