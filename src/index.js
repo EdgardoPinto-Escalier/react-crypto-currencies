395f11af163a07dddd72746501074be0f6a2afdc
EdgardoPinto-Escalier <ep-marketing-media@live.com>
2020-03-02 20:35:35 +0100

    Created the detail component

--- src/index.js
+++ src/index.js
@@ -4,6 +4,7 @@ import { BrowserRouter, Route, Switch } from 'react-router-dom';
 import Header from './components/common/Header';
 import List from './components/list/List';
 import NotFound from './components/notfound/NotFound';
+import Detail from './components/detail/Detail';
 import './index.css';
 
 
@@ -15,6 +16,7 @@ const App = () => {
 
         <Switch>
           <Route path="/" component={List} exact />
+          <Route path="/currency/:id" component={Detail} exact />
           <Route component={NotFound} />
         </Switch>
       </div>
